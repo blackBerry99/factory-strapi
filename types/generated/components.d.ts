6a5b9a81708d572f7daeec0a9727233fb07d1b44
Yevheniia Nikolska <yevheniia.nikolska@otakoyi.com>
2025-02-09 23:21:11 +0200

    feat: added blocks

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,69 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface BlocksEventSlider extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_event_sliders';
+  info: {
+    displayName: 'EventSlider';
+  };
+  attributes: {
+    events: Schema.Attribute.Relation<'oneToMany', 'api::event.event'>;
+  };
+}
+
+export interface BlocksEventsList extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_events_lists';
+  info: {
+    displayName: 'EventsList';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    events: Schema.Attribute.Relation<'oneToMany', 'api::event.event'>;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface BlocksHistory extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_histories';
+  info: {
+    displayName: 'History';
+    icon: 'bulletList';
+  };
+  attributes: {
+    history_items: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::history-item.history-item'
+    >;
+    shortDescription: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface BlocksTextImage extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_text_images';
+  info: {
+    description: '';
+    displayName: 'TextImage';
+  };
+  attributes: {
+    bgDark: Schema.Attribute.Boolean;
+    description: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<'files' | 'images'>;
+    ImageFirst: Schema.Attribute.Boolean;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface BlocksVideo extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_videos';
+  info: {
+    displayName: 'Video';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    videoUrl: Schema.Attribute.Text;
+  };
+}
+
 export interface SharedMedia extends Struct.ComponentSchema {
   collectionName: 'components_shared_media';
   info: {
@@ -75,6 +139,11 @@ export interface SharedSocial extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'blocks.event-slider': BlocksEventSlider;
+      'blocks.events-list': BlocksEventsList;
+      'blocks.history': BlocksHistory;
+      'blocks.text-image': BlocksTextImage;
+      'blocks.video': BlocksVideo;
       'shared.media': SharedMedia;
       'shared.quote': SharedQuote;
       'shared.rich-text': SharedRichText;
